08f64aad05e4ec0aaf2c776cd41b129e9f506ecc
Cedric Mathis <clmathis@uh.edu>
2020-02-24 17:48:44 -0600

    set up new packages and added sessions

--- Server/app.js
+++ Server/app.js
@@ -1,15 +1,32 @@
 const express = require('express');
+const request = require('request');
+const session = require('client-sessions');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const formidableMiddleware = require('express-formidable');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// Routes
+
 // middleware
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 
 // Dummy Route To return something on LocalHost:9000
 app.get("/",function(req,res){    
     res.send("home");
 });
+
+app.use(session({
+    cookieName: 'session',
+    secret: 'Charlotte',
+    duration: 30 * 60 * 100,
+    activeDuration: 5 * 60 * 1000,
+    ephemeral: true
+}));
 
 // Respond with error message on error
 app.use(function(err, req, res, next) {
@@ -19,6 +36,8 @@ app.use(function(err, req, res, next) {
         error : {}
     });
 });
+
+
 
 // Set Port Number and run server with yarn run start nodemon will
 let port = 9000;
